d3516a4e906a5d52917b964a78523b03dd844429
vishalnarra1 <158012115+vishalnarra1@users.noreply.github.com>
2024-03-19 15:38:03 -0500

    Add files via upload

---
+++ GameTestApp/GameTestApp macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  GameTestApp macOS
+//
+//  Created by Narra, Vishal on 3/19/24.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
