875c6c01b3259ee4a3e8e9ad61fdfe4feedb1c81
vishalnarra1 <158012115+vishalnarra1@users.noreply.github.com>
2024-03-19 15:05:52 -0500

    Add files via upload

---
+++ GamePlanning.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var all = [1,2,3,4,5,6,7,8,9]
+
+var winningMoves = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+
+func checkWinners(submittedMoves:[Int])->Bool{
+    
+    for moves in winningMoves{
+        if moves.allSatisfy(submittedMoves.contains){
+            return true
+        }
+    }
+    return false
+    
+}
+
+print(checkWinners(submittedMoves: [1,5,6,9]))
