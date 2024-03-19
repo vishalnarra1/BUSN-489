77a28b4a18a4e9a7c41301ccbfda2ca771207846
vishalnarra1 <158012115+vishalnarra1@users.noreply.github.com>
2024-03-19 15:06:14 -0500

    Add files via upload

---
+++ MyPlayground1.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+var environment = "development"
+let maximumNumberOfLoginAttempts1: Int
+
+if environment == "development"{
+    maximumNumberOfLoginAttempts1 = 100
+} else {
+    maximumNumberOfLoginAttempts1 = 10
+}
+
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage: String
+
+welcomeMessage = "Hello"
+
+var red,green,blue: Double
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+
+var languageName = "Swift"
+languageName = "Swift++"
+
+print(friendlyWelcome)
+
+print("The current value of friendlyWelcome is \(friendlyWelcome)")
+//comment line test
+
+let cat = "🐱"; print(cat)
+
