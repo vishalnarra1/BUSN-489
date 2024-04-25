dbf5cbd8d7a0a800d454f5183c96f9c33926d61a
vishalnarra1 <158012115+vishalnarra1@users.noreply.github.com>
2024-04-25 00:01:40 -0500

    Add files via upload

---
+++ Playground8.playground/Contents.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+class Person {
+    var residence: Residence?
+}
+
+
+class Residence {
+    var numberOfRooms = 1
+}
+
+let john = Person()
+
+let roomCount = john.residence!.numberOfRooms
+// this triggers a runtime error
+
+if let roomCount = john.residence?.numberOfRooms {
+    print("John's residence has \(roomCount) room(s).")
+} else {
+    print("Unable to retrieve the number of rooms.")
+}
+// Prints "Unable to retrieve the number of rooms."
+
+if let roomCount = john.residence?.numberOfRooms {
+    print("John's residence has \(roomCount) room(s).")
+} else {
+    print("Unable to retrieve the number of rooms.")
+}
+// Prints "John's residence has 1 room(s)."
+
+class Person {
+    var residence: Residence?
+}
+
+class Residence {
+    var rooms: [Room] = []
+    var numberOfRooms: Int {
+        return rooms.count
+    }
+    subscript(i: Int) -> Room {
+        get {
+            return rooms[i]
+        }
+        set {
+            rooms[i] = newValue
+        }
+    }
+    func printNumberOfRooms() {
+        print("The number of rooms is \(numberOfRooms)")
+    }
+    var address: Address?
+}
+
+class Address {
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    func buildingIdentifier() -> String? {
+        if let buildingNumber = buildingNumber, let street = street {
+            return "\(buildingNumber) \(street)"
+        } else if buildingName != nil {
+            return buildingName
+        } else {
+            return nil
+        }
+    }
+}
